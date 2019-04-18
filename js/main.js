b992026040e73a2a22448b234df8bb8d39b228d6
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-04-18 16:20:31 +0000

    Add more features

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(255, 0, 255)",
-  "rgb(0, 0, 128)",
-  "rgb(0, 128, 128)",
-  "rgb(128, 0, 128)"
-];
+
+let h1 = document.querySelector('h1');
+
+let randomColor = () => {
+  //pick a "red", "green" and "blue" from 0 - 255
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+let generateRandomColors = (num) => {
+  let arr = [];
+  for(let i = 0; i < num; i++) {
+    arr.push(randomColor()); 
+  }
+  return arr;
+}
+
+let colors = generateRandomColors(9);
 
 let pickColor = () => {
   let random = Math.floor(Math.random() * colors.length);
@@ -19,6 +28,17 @@ let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#messageDisplay");
+let resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener('click', () => {
+  colors = generateRandomColors(9);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+})
+
 
 colorDisplay.textContent = pickedColor;
 for(let i = 0; i < squares.length; i++){
@@ -27,7 +47,9 @@ for(let i = 0; i < squares.length; i++){
     let clickedColor = this.style.backgroundColor;
     if(clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct, Hurray!!!";
+      resetBtn.textContent = "Play Again";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = 'Hoops, Try again!';
@@ -40,6 +62,8 @@ let changeColors = (color) => {
     squares[i].style.backgroundColor = color;
   }
 }
+
+
 
 
 
