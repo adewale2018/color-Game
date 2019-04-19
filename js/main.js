8326ac24667f3ff26b023d2a058fa9a214af4c5a
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-04-19 10:43:00 +0000

    Add more features

--- js/main.js
+++ js/main.js
@@ -1,5 +1,60 @@
-
+let numOfSquares = 9;
 let h1 = document.querySelector('h1');
+let modeButtons = document.querySelectorAll('.mode');
+for(let i = 0; i < modeButtons.length; i++) {
+  modeButtons[i].addEventListener('click', function() {
+    modeButtons[0].classList.remove("selected");
+    modeButtons[1].classList.remove("selected");
+    this.classList.add("selected");
+    this.textContent === "Easy" ? numOfSquares = 6 : numOfSquares = 9;
+    reset();
+  });
+}
+
+function reset() {
+  colors = generateRandomColors(numOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(let i = 0; i < squares.length; i++) {
+    if(colors[i]) {
+      squares[i].style.display = "block";
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+  h1.style.backgroundColor = "steelblue";
+  messageDisplay.textContent = "";
+  resetBtn.textContent = "New Colors";
+}
+// easyBtn.addEventListener('click', () => {
+//   easyBtn.classList.add('selected');
+//   hardBtn.classList.remove('selected');
+//   numOfSquares = 6;
+//   colors = generateRandomColors(numOfSquares);
+//   pickedColor = pickColor(); 
+//   colorDisplay.textContent = pickedColor;
+//   for(let i = 0; i < squares.length; i++) {
+//     if(colors[i]){
+//       squares[i].style.backgroundColor = colors[i];
+//     } else {
+//       squares[i].style.display = "none";
+//     }
+//   }
+// });
+
+// hardBtn.addEventListener('click', () => {
+//   easyBtn.classList.remove('selected');
+//   hardBtn.classList.add('selected');
+//   numOfSquares = 9;
+//   colors = generateRandomColors(numOfSquares);
+//   pickedColor = pickColor(); 
+//   colorDisplay.textContent = pickedColor;
+//   for(let i = 0; i < squares.length; i++) {
+//       squares[i].style.backgroundColor = colors[i];
+//       squares[i].style.display = "block";
+//   }
+// });
 
 let randomColor = () => {
   //pick a "red", "green" and "blue" from 0 - 255
@@ -17,7 +72,7 @@ let generateRandomColors = (num) => {
   return arr;
 }
 
-let colors = generateRandomColors(9);
+let colors = generateRandomColors(numOfSquares);
 
 let pickColor = () => {
   let random = Math.floor(Math.random() * colors.length);
@@ -29,14 +84,9 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#messageDisplay");
 let resetBtn = document.querySelector("#reset");
+
 resetBtn.addEventListener('click', () => {
-  colors = generateRandomColors(9);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-  for(let i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-  }
-  h1.style.backgroundColor = "#232323";
+  reset();
 })
 
 
